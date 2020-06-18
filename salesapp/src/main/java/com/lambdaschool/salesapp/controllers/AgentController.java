b44ea3dae6da612812caf5f6ba48d13b8b51cccc
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-18 19:08:24 +0800

    Add '/agents/agent/{id}' route

--- salesapp/src/main/java/com/lambdaschool/salesapp/controllers/AgentController.java
+++ salesapp/src/main/java/com/lambdaschool/salesapp/controllers/AgentController.java
@@ -1,6 +1,13 @@
 package com.lambdaschool.salesapp.controllers;
 
 
+import com.lambdaschool.salesapp.models.Agent;
+import com.lambdaschool.salesapp.services.AgentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/agents")
 public class AgentController {
 
+    @Autowired
+    private AgentService agentService;
+
     // http://localhost:2019/agents/agent/9
+    @GetMapping(value = "/agent/{id}", produces = "application/json")
+    public ResponseEntity<?> findAgentById(@PathVariable long id) {
+
+        Agent a = agentService.findByAgentId(id);
+        return new ResponseEntity<>(a, HttpStatus.OK);
+    }
 }
