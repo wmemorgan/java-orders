cfdd5cbbd407ba021e529cb3839a3d68c12b5a3d
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-14 02:40:18 +0800

    Initialize project

---
+++ crudyorders/src/main/java/com/lambdaschool/crudyorders/CrudyordersApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.crudyorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudyordersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudyordersApplication.class, args);
+    }
+
+}
